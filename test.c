8723fb1919dd6d97e8295baeb07fbb063c727462
hyperfunny88 <hyperfunny88@proton.me>
2023-06-03 05:10:33 +0100

    test updates

--- test.c
+++ test.c
@@ -4,10 +4,12 @@
 int main(void)
 {
 	Lt lt;
-	lt_make(&lt, 100, 0.85f);
-	for (int i = 0 ;i < 100; ++i) {
-		printf("%u/%u, %u\n", lt.sz, lt.lsz, lt.msz);
+	lt_make(&lt, 5, 0.85f);
+	for (int i = 0; i < 100; ++i) {
 		printf("%p\n", lt_add(&lt, i, i));
+		printf("%p\n", lt_find(&lt, i));
+	}
+	for (int i = 0; i < 100; ++i) {
 		printf("%p\n", lt_find(&lt, i));
 	}
 	lt_rm(&lt, lt_find(&lt, 50));
